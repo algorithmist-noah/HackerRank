2bed939879b97c50e01cf2bc320ec579e43ea33a
algorithmist-ns <algorithmist.ns@gmail.com>
2019-09-01 21:25:00 -0500

    Another slight improvement.

--- ProjectEuler/001 - Multiples of 3 and 5/Program.cs
+++ ProjectEuler/001 - Multiples of 3 and 5/Program.cs
@@ -4,14 +4,14 @@ class Solution
 {
     static void Main(String[] args)
     {
+        BigInteger totalSum = 0;
+
         int t = Convert.ToInt32(Console.ReadLine());
         for (int a0 = 0; a0 < t; a0++)
         {
             int n = Convert.ToInt32(Console.ReadLine());
 
             int modifiedN = n - 1;
-
-            BigInteger totalSum = 0;
 
             BigInteger factor3 = modifiedN / 3;
             BigInteger factor5 = modifiedN / 5;
@@ -25,6 +25,8 @@ class Solution
             totalSum -= 15 * (factor15 * (factor15 + 1) / 2);
 
             Console.WriteLine(totalSum);
+
+            totalSum = 0;
         }
     }
 }
