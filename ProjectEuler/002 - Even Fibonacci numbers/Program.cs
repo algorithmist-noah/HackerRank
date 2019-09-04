eda49c6bd72af102310785e7287cd00d442a97a1
algorithmist-ns <algorithmist.ns@gmail.com>
2019-09-03 22:27:24 -0500

    Commit.

--- ProjectEuler/002 - Even Fibonacci numbers/Program.cs
+++ ProjectEuler/002 - Even Fibonacci numbers/Program.cs
@@ -1,30 +1,32 @@
 using System;
 using System.Numerics;
-
-namespace TestProject
+class Solution
 {
-    class Program
+
+    static void Main(String[] args)
     {
-        static void Main(string[] args)
+        int t = Convert.ToInt32(Console.ReadLine());
+        for (int a0 = 0; a0 < t; a0++)
         {
-            int t = Convert.ToInt32(Console.ReadLine());
-            for (int a0 = 0; a0 < t; a0++)
+            long n = Convert.ToInt64(Console.ReadLine());
+
+            long prev = 1, current = 1;
+
+            BigInteger totalSum = 0;
+
+            for (long fibSum = 1; fibSum < n;)
             {
-                long n = Convert.ToInt64(Console.ReadLine());
-
-                //1,2,3,5,8,13,21,34,55,89,144,233,377,610
-                //2,8,34,144,610
-                //2,5,8,11,14
-
-                BigInteger fibSum = 1, prev = 1, current = 1;
-
-                for (long i = 1; i < n; i++)
+                if (fibSum % 2 == 0)
                 {
-                    fibSum = prev + current;
-                    prev = current;
-                    current = fibSum;
+                    totalSum += fibSum;
                 }
+
+                fibSum = prev + current;
+                prev = current;
+                current = fibSum;
             }
+
+            Console.WriteLine(totalSum);
         }
     }
 }
