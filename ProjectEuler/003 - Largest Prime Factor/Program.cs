4465ffdc55090f1983ab7f89830b555383b88769
algorithmist-ns <algorithmist.ns@gmail.com>
2019-09-04 23:32:47 -0500

    Commit for solution to 003.

---
+++ ProjectEuler/003 - Largest Prime Factor/Program.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace TestProject
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            for (int a0 = 0; a0 < t; a0++)
+            {
+                long n = Convert.ToInt64(Console.ReadLine());
+
+                var limit = Math.Sqrt(n) + 1;
+
+                if (n % 2 == 0)
+                {
+                    while (n % 2 == 0)
+                    {
+                        n /= 2;
+                    }
+                }
+
+                for (long i = 3; i < limit; i += 2)
+                {
+                    while (n % i == 0 && i != n)
+                    {
+                        n /= i;
+                    }
+                }
+
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
