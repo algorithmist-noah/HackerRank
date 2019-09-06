b90ac7e7448c76585291393df65e12ac6f9ec2a9
algorithmist-ns <algorithmist.ns@gmail.com>
2019-09-05 21:54:16 -0500

    Commit for solution on 004.

---
+++ ProjectEuler/004 - Largest palindome product/Program.cs
@@ -0,0 +1,48 @@
+using System;
+
+namespace TestProject
+{
+    class Program
+    {
+        static bool CheckIfPalindome(string str)
+        {
+            var isPalindrome = true;
+
+            for (int i = 0, j = str.Length - 1; i < j; i++, j--)
+            {
+                if (str[i] != str[j])
+                {
+                    return false;
+                }
+            }
+
+            return isPalindrome;
+        }
+
+        static void Main(String[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            for (int a0 = 0; a0 < t; a0++)
+            {
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                var largestPalindrome = 101101;
+
+                for (int i = 100; i < 999; i++)
+                {
+                    for (int j = 100; j < 999; j++)
+                    {
+                        var multiple = i * j;
+
+                        if (multiple > largestPalindrome && multiple < n && CheckIfPalindome(multiple.ToString()))
+                        {
+                            largestPalindrome = multiple;
+                        }
+                    }
+                }
+
+                Console.WriteLine(largestPalindrome);
+            }
+        }
+    }
+}
