08561cccaa3e9f0184587227021af46fcec13db6
algorithmist-ns <algorithmist.ns@gmail.com>
2019-09-08 20:23:35 -0500

    Daily commit.

---
+++ ProjectEuler/006 - Sum square difference/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        int t = Convert.ToInt32(Console.ReadLine());
+        for (int a0 = 0; a0 < t; a0++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
