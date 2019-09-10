ff21cb28daceecdedc85b4a3a701ae2d02d79c38
algorithmist-ns <algorithmist.ns@gmail.com>
2019-09-09 21:43:26 -0500

    Solution for 006.

--- ProjectEuler/006 - Sum square difference/Program.cs
+++ ProjectEuler/006 - Sum square difference/Program.cs
@@ -1,16 +1,18 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 class Solution
 {
-
     static void Main(String[] args)
     {
         int t = Convert.ToInt32(Console.ReadLine());
         for (int a0 = 0; a0 < t; a0++)
         {
             int n = Convert.ToInt32(Console.ReadLine());
+
+            long partialProduct = n * (n + 1) / 2;
+
+            long squareDifference = (long)Math.Pow(partialProduct, 2) - partialProduct * (2 * n + 1) / 3;
+
+            Console.WriteLine(squareDifference);
         }
     }
 }
