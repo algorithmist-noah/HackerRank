f660f5b6e7f2cb36ef3e52b628f2fcc53630949e
algorithmist-ns <algorithmist.ns@gmail.com>
2019-09-11 21:46:04 -0500

    Daily commit.

--- ProjectEuler/005 - Smallest multiple/Program.cs
+++ ProjectEuler/005 - Smallest multiple/Program.cs
@@ -5,6 +5,7 @@ class Solution
     static void Main(String[] args)
     {
         int t = Convert.ToInt32(Console.ReadLine());
+
         for (int a0 = 0; a0 < t; a0++)
         {
             int n = Convert.ToInt32(Console.ReadLine());
