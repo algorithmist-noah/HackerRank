e0631e372c3fc39cc36eb6b76bff953085fc2017
algorithmist-ns <algorithmist.ns@gmail.com>
2019-09-18 00:46:10 -0500

    Daily commit.

---
+++ ProjectEuler/011 - Largest product in a grid/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        int[][] grid = new int[20][];
+        for (int grid_i = 0; grid_i < 20; grid_i++)
+        {
+            string[] grid_temp = Console.ReadLine().Split(' ');
+            grid[grid_i] = Array.ConvertAll(grid_temp, Int32.Parse);
+        }
+    }
+}
